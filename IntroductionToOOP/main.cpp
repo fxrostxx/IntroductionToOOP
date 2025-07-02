cbd02fa7daddb918f8a4a9bbfbd37ca2a0ece843
fxrostxx <fxrostxx@FXROSTXX-PC>
2025-07-02 20:36:04 +0300

    SpecialMembers DONE

--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -1,6 +1,11 @@
 ﻿#include <iostream>
 #include <cmath>
 using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
+
+#define delimiter "\n--------------------------------------------------\n"
 
 double square(double n);
 
@@ -30,6 +35,43 @@ public:
 		return y;
 	}
 
+	/*Point()
+	{
+		x = y = 0;
+		cout << "DefaultConstructor: " << this << endl;
+	}
+	Point(double x)
+	{
+		this->x = x;
+		y = 0;
+		cout << "SingleArgumentConstructor: " << this << endl;
+	}*/
+	Point(double x = 0, double y = 0)
+	{
+		this->x = x;
+		this->y = y;
+		cout << "Constructor: " << this << endl;
+	}
+	Point(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyConstructor: " << this << endl;
+	}
+	~Point()
+	{
+		cout << "Destructor: " << this << endl;
+	}
+
+	Point& operator = (const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyAssignment: " << this << endl;
+
+		return *this;
+	}
+
 	double distance() const // Расстояние до точки от начала координат
 	{
 		return sqrt(square(x) + square(y));
@@ -39,13 +81,19 @@ public:
 	{
 		return sqrt(square(x - B.x) + square(y - B.y));
 	}
+
+	void print() const
+	{
+		cout << this << ": x = " << x << ", y = " << y << endl;
+	}
 };
 
 double distance(const Point& A, const Point& B); // Расстояние между двумя точками
 
 //#define STRUCT_POINT
 //#define STRUCT_POINT_GET_SET
-#define DISTANCE
+//#define DISTANCE
+//#define CONSTRUCTORS
 
 int main()
 {
@@ -80,14 +128,49 @@ int main()
 
 	cout << "Расстояние до точки A от начала координат: " << A.distance() << endl;
 
+	cout << delimiter << endl;
+
 	cout << "Введите координату x точки B: "; cin >> x;
 	cout << "Введите координату y точки B: "; cin >> y;
 	B.set_x(x);
 	B.set_y(y);
 
 	cout << "Расстояние от точки A до точки B (функция): " << distance(A, B) << endl;
+
+	cout << delimiter << endl;
+
 	cout << "Расстояние от точки A до точки B (метод): " << A.distance(B) << endl;
 #endif // DISTANCE
+
+#ifdef CONSTRUCTORS
+	Point A;
+	A.print();
+
+	Point B{ 5 };
+	B.print();
+
+	Point C{ 7, 8 };
+	C.print();
+
+	Point D{ C };
+	D.print();
+
+	Point E;
+	E = D;
+	E.print();
+#endif // CONSTRUCTORS
+
+	int a, b, c;
+	a = b = c = 0;
+	cout << a << ' ' << b << ' ' << c << endl;
+
+	Point A, B, C;
+	cout << delimiter << endl;
+	A = B = C = Point{ 2, 3 };
+	cout << delimiter << endl;
+	A.print();
+	B.print();
+	C.print();
 
 	return 0;
 }
