a52ed4b8969f794749f399360c18385eeb4eb28d
fxrostxx <fxrostxx@FXROSTXX-PC>
2025-07-05 13:02:37 +0300

    FractionOperators DONE

--- Fraction/main.cpp
+++ Fraction/main.cpp
@@ -1,5 +1,15 @@
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
+
+int GCD(int a, int b)
+{
+	if (a == 0) return b;
+	return GCD(b % a, a);
+}
+int LCM(int a, int b)
+{
+	return (a * b) / GCD(a, b);
+}
 
 class Fraction
 {
@@ -91,6 +101,112 @@ public:
 
 		return *this;
 	}
+	Fraction& operator+= (const Fraction& other)
+	{
+		if (denominator == other.denominator)
+		{
+			integer += other.integer;
+			numerator += other.get_numerator();
+		}
+		else
+		{
+			int lcm{ LCM(denominator, other.denominator) };
+
+			integer += other.integer;
+			numerator = numerator * (lcm / denominator) +
+				other.numerator * (lcm / other.denominator);
+			denominator = lcm;
+		}
+
+		this->ToProper();
+
+		return *this;
+	}
+	Fraction& operator-= (const Fraction& other)
+	{
+		if (denominator == other.denominator)
+		{
+			integer -= other.integer;
+			numerator -= other.get_numerator();
+		}
+		else
+		{
+			int lcm{ LCM(denominator, other.denominator) };
+
+			integer -= other.integer;
+			numerator = numerator * (lcm / denominator) -
+				other.numerator * (lcm / other.denominator);
+			denominator = lcm;
+		}
+
+		this->ToProper();
+
+		return *this;
+	}
+	Fraction& operator*= (Fraction other)
+	{
+		this->ToImproper();
+		other.ToImproper();
+
+		numerator *= other.numerator;
+		denominator *= other.denominator;
+
+		this->ToProper();
+
+		return *this;
+	}
+	Fraction& operator/= (Fraction other)
+	{
+		this->ToImproper();
+		other.ToImproper();
+
+		numerator *= other.denominator;
+		denominator *= other.numerator;
+
+		this->ToProper();
+
+		return *this;
+	}
+	Fraction& operator++ ()
+	{
+		this->ToImproper();
+
+		numerator += denominator;
+
+		this->ToProper();
+
+		return *this;
+	}
+	Fraction operator++ (int)
+	{
+		Fraction old = *this;
+
+		this->ToImproper();
+		numerator += denominator;
+		this->ToProper();
+
+		return old;
+	}
+	Fraction& operator-- ()
+	{
+		this->ToImproper();
+
+		numerator -= denominator;
+
+		this->ToProper();
+
+		return *this;
+	}
+	Fraction operator-- (int)
+	{
+		Fraction old = *this;
+
+		this->ToImproper();
+		numerator -= denominator;
+		this->ToProper();
+
+		return old;
+	}
 
 	void Print() const
 	{
@@ -118,6 +234,15 @@ public:
 
 		return *this;
 	}
+	Fraction& ReduceFraction()
+	{
+		int gcd{ GCD(numerator, denominator) };
+
+		numerator /= gcd;
+		denominator /= gcd;
+
+		return *this;
+	}
 };
 
 Fraction operator* (Fraction left, Fraction right)
@@ -130,6 +255,106 @@ Fraction operator* (Fraction left, Fraction right)
 		left.get_numerator() * right.get_numerator(),
 		left.get_denominator() * right.get_denominator()
 	}.ToProper();
+}
+Fraction operator/ (Fraction left, Fraction right)
+{
+	left.ToImproper();
+	right.ToImproper();
+
+	return Fraction
+	{
+		left.get_numerator() * right.get_denominator(),
+		left.get_denominator() * right.get_numerator()
+	}.ToProper();
+}
+Fraction operator+ (const Fraction& left, const Fraction& right)
+{
+	if (left.get_denominator() == right.get_denominator()) return Fraction
+	{
+		left.get_integer() + right.get_integer(),
+		left.get_numerator() + right.get_numerator(),
+		left.get_denominator()
+	};
+
+	int lcm{ LCM(left.get_denominator(), right.get_denominator()) };
+
+	return Fraction
+	{
+		left.get_integer() + right.get_integer(),
+
+		left.get_numerator() * (lcm / left.get_denominator()) +
+		right.get_numerator() * (lcm / right.get_denominator()),
+
+		lcm
+	}.ToProper();
+}
+Fraction operator- (const Fraction& left, const Fraction& right)
+{
+	if (left.get_denominator() == right.get_denominator()) return Fraction
+	{
+		left.get_integer() - right.get_integer(),
+		left.get_numerator() - right.get_numerator(),
+		left.get_denominator()
+	};
+
+	int lcm{ LCM(left.get_denominator(), right.get_denominator()) };
+
+	return Fraction
+	{
+		left.get_integer() - right.get_integer(),
+
+		left.get_numerator() * (lcm / left.get_denominator()) -
+		right.get_numerator() * (lcm / right.get_denominator()),
+
+		lcm
+	}.ToProper();
+}
+bool operator== (const Fraction& left, const Fraction& right)
+{
+	return left.get_integer() == right.get_integer()
+		&& left.get_numerator() == right.get_numerator()
+		&& left.get_denominator() == right.get_denominator();
+}
+bool operator!= (const Fraction& left, const Fraction& right)
+{
+	return !(left == right);
+}
+bool operator> (Fraction left, Fraction right)
+{
+	if (left == right) return false;
+
+	/*else if (left.get_denominator() == right.get_denominator())
+		return left.get_integer() >= right.get_integer() && left.get_numerator() > right.get_numerator();
+
+	else if (left.get_numerator() == right.get_numerator())
+		return left.get_integer() >= right.get_integer() && left.get_denominator() < right.get_denominator();
+
+	else if (left.get_denominator() == right.get_denominator() && left.get_numerator() == right.get_numerator())
+		return left.get_integer() > right.get_integer();*/
+
+	else
+	{
+		left.ToImproper();
+		right.ToImproper();
+
+		int lcm{ LCM(left.get_denominator(), right.get_denominator()) };
+
+		return left.get_numerator() * (lcm / left.get_denominator()) >
+			right.get_numerator() * (lcm / right.get_denominator());
+	}
+}
+bool operator< (const Fraction& left, const Fraction& right)
+{
+	if (left == right) return false;
+	else return !(left > right);
+}
+bool operator>= (const Fraction& left, const Fraction& right)
+{
+	return !(left < right);
+}
+bool operator <= (const Fraction& left, const Fraction& right)
+{
+	return !(left > right);
 }
 
 //#define CONSTRUCTORS
@@ -161,7 +386,63 @@ int main()
 	B.Print();
 
 	Fraction C{ A * B };
-	C.Print();
+	cout << "Произведение: ";  C.Print();
+
+	C = A / B;
+	cout << "Частное: ";  C.Print();
+
+	C = A + B;
+	cout << "Сумма: ";  C.Print();
+
+	C = A - B;
+	cout << "Разность: ";  C.Print();
+
+	A += B;
+	cout << "Сумма (+=): ";  A.Print();
+
+	Fraction D{ 1, 2, 3 };
+	Fraction E{ 2, 3, 4 };
+
+	D -= E;
+	cout << "Разность (-=): ";  D.Print();
+
+	Fraction F{ 1, 2, 3 };
+	Fraction G{ 2, 3, 4 };
+
+	F *= G;
+	cout << "Произведение (*=): ";  F.Print();
+
+	Fraction H{ 1, 2, 3 };
+	Fraction I{ 2, 3, 4 };
+
+	H /= I;
+	cout << "Частное (/=): ";  H.Print();
+
+	I = ++H;
+	cout << "Префиксный инкремент: ";  I.Print();
+
+	I = H++;
+	cout << "Постфиксный инкремент: ";  I.Print();
+
+	Fraction J{ 1, 5, 3 };
+	Fraction K{ 1, 2, 3 };
+
+	cout << "Оператор сравнения (==): " << (J == K) << endl;
+
+	cout << "Оператор сравнения (!=): " << (J != K) << endl;
+
+	cout << "Оператор сравнения (>): " << (J > K) << endl;
+
+	cout << "Оператор сравнения (<): " << (J < K) << endl;
+
+	cout << "Оператор сравнения (>=): " << (J >= K) << endl;
+
+	cout << "Оператор сравнения (<=): " << (J <= K) << endl;
+
+	Fraction L{ 1, 15, 3 };
+
+	L.ReduceFraction();
+	cout << "Сокращение дроби: "; L.Print();
 
 	return 0;
 }
