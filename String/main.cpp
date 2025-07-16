9e102d559ff82d2ce782629d0228f3b2cbd40971
fxrostxx <fxrostxx@FXROSTXX-PC>
2025-07-16 19:53:45 +0300

    Subscript operator in String DONE

--- String/main.cpp
+++ String/main.cpp
@@ -80,14 +80,22 @@ public:
 
 		return *this;
 	}
+	char operator[] (int i) const
+	{
+		return str[i];
+	}
+	char& operator[] (int i)
+	{
+		return str[i];
+	}
 };
 
 String operator+ (const String& left, const String& right)
 {
 	String result{ left.get_size() + right.get_size() - 1 };
 
-	for (int i = 0; i < left.get_size(); ++i) result.get_str()[i] = left.get_str()[i];
-	for (int i = 0; i < right.get_size(); ++i) result.get_str()[i + left.get_size() - 1] = right.get_str()[i];
+	for (int i = 0; i < left.get_size(); ++i) result[i] = left[i];
+	for (int i = 0; i < right.get_size(); ++i) result[i + left.get_size() - 1] = right[i];
 
 	return result;	
 }
